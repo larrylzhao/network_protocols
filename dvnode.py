13473dfff18dc25495840c17ce6085e8eb0e92c3
larryzhao <larrylzhao@gmail.com>
2017-07-02 22:37:49 -0500

    routing table sending and receiving done

--- dvnode.py
+++ dvnode.py
@@ -15,19 +15,49 @@ import re
 from socket import *
 import datetime
 import random
+import json
+import unicodedata
 
-
+ip = "localhost"
 localPort = 0
 routingTable = {}
+listensocket = socket(AF_INET, SOCK_DGRAM)
 
 """
 function to print the routing table
 """
-def print_routing_table():
-    global routingTable
-    print "Node " + str(localPort) + " Routing Table"
+def print_routing_table(port, table):
+    print "Node " + str(port) + " Routing Table"
+    for key in table:
+        print key, "- (" + str(table[key]['weight']) + " -> " + str(key) + " ; Next hop -> Node " + str(table[key]['next'])
+
+
+"""
+Thread for listening to incoming UDP messages
+"""
+def listen():
+    global localPort, listensocket
+
+    while True:
+        # data schema: neighborPort;serialized json object for neighbor's routing table
+        data, sender = listensocket.recvfrom(1024)
+        datasplit = data.split(";")
+        neighborPort = datasplit[0]
+        neighborTable = json.loads(datasplit[1])
+        print_routing_table(neighborPort, neighborTable)
+
+
+"""
+Function to send table
+"""
+def send_table(table):
+    global ip, localPort
+
+    sSocket = socket(AF_INET, SOCK_DGRAM)
     for key in routingTable:
-        print "- (" + str(routingTable[key]['weight']) + " -> " + str(key) + " ; Next hop -> Node " + str(routingTable[key]['next'])
+        print "sending table to " + str(key)
+        sSocket.sendto(str(localPort) + ";" + json.dumps(table, ensure_ascii=False), (ip,int(key)))
+    sSocket.close
 
 
 def main():
@@ -43,32 +73,35 @@ def main():
     goodArgs = True
     if len(sys.argv) > 1:
         localPort = int(sys.argv[1])
-        if 1024 <= localPort <= 65535:
+        if 1025 <= localPort <= 65535:
             print "local port number:", localPort
         else:
-            print "please give a local port number between 1024 and 65535"
+            print "please give a local port number between 1025 and 65535"
             print usage
             exit()
     else:
         print usage
         exit()
+
     # parse neighbors into routing table
     if len(sys.argv) < 3:
         print "please provide at least one neighbor"
         print usage
         exit()
-    # routingTable[localPort] = {}
+
+    last = False
     for i in range(2, len(sys.argv))[::2]:
         if sys.argv[i] == "last":
-            #TODO add method for kickstarting algo
+            last = True
             break
 
         neighborPort = int(sys.argv[i])
-        if localPort < 1024 or localPort > 65535:
-            print "please give a neighbor port number between 1024 and 65535"
+        if localPort < 1025 or localPort > 65535:
+            print "please give a neighbor port number between 1025 and 65535"
             print usage
             exit()
         try:
+            neighborPort = str(neighborPort)
             neighborWeight = float(sys.argv[i+1])
             if neighborWeight < 0.0 or neighborWeight > 1.0:
                 print "please provide a valid loss rate for neighbor", neighborPort
@@ -80,7 +113,26 @@ def main():
             print usage
             print str(e)
             exit()
-    print_routing_table()
+    print_routing_table(localPort, routingTable)
+
+    try:
+        # start thread to listen to inbound traffic
+        listensocket.bind(('', localPort))
+        listenthread = threading.Thread(target=listen, args=())
+        listenthread.daemon = True
+        listenthread.start()
+
+        if last is True:
+            send_table(routingTable)
+
+        while True:
+            pass
+        exit()
+    except (KeyboardInterrupt):
+        listensocket.close()
+        print "\n[exiting]"
+        exit()
+
 
 
 
